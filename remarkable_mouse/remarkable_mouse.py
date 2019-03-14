f72880a9b64799030dd5b80137b2005cf48a3654
evan <evan@evanw.org>
2019-03-14 12:28:32 -0500

    relative movement working

--- remarkable_mouse/remarkable_mouse.py
+++ remarkable_mouse/remarkable_mouse.py
@@ -19,17 +19,19 @@ e_type_abs = 3
 # evcode_stylus_distance = 25
 # evcode_stylus_xtilt = 26
 # evcode_stylus_ytilt = 27
-e_code_stylus_xpos = 1
-e_code_stylus_ypos = 0
-e_code_stylus_pressure = 24
-# evcode_finger_xpos = 53
-# evcode_finger_ypos = 54
-# evcode_finger_pressure = 58
+# e_code_stylus_xpos = 1
+# e_code_stylus_ypos = 0
+# e_code_stylus_pressure = 24
+e_code_finger_xpos = 53
+e_code_finger_ypos = 54
+e_code_finger_pressure = 58
+e_code_finger_touch = 57
+e_value_finger_up = -1
 
-stylus_width = 15725
-stylus_height = 20951
-# finger_width = 767
-# finger_height = 1023
+# stylus_width = 15725
+# stylus_height = 20951
+finger_width = 767
+finger_height = 1023
 
 mouse = Controller()
 logging.basicConfig(format='%(message)s')
@@ -37,28 +39,28 @@ log = logging.getLogger(__name__)
 
 
 # remap wacom coordinates in various orientations
-def fit(x, y, stylus_width, stylus_height, monitor, orientation):
+def fit(x, y, finger_width, finger_height, monitor, orientation):
 
     if orientation == 'vertical':
-        y = stylus_height - y
+        y = finger_height - y
     elif orientation == 'right':
         x, y = y, x
-        stylus_width, stylus_height = stylus_height, stylus_width
+        finger_width, finger_height = finger_height, finger_width
     elif orientation == 'left':
-        x, y = stylus_height - y, stylus_width - x
-        stylus_width, stylus_height = stylus_height, stylus_width
+        x, y = finger_height - y, x
+        finger_width, finger_height = finger_height, finger_width
 
-    ratio_width, ratio_height = monitor.width / stylus_width, monitor.height / stylus_height
+    ratio_width, ratio_height = monitor.width / finger_width, monitor.height / finger_height
     scaling = ratio_width if ratio_width > ratio_height else ratio_height
 
     return (
-        scaling * (x - (stylus_width - monitor.width / scaling) / 2),
-        scaling * (y - (stylus_height - monitor.height / scaling) / 2)
+        scaling * (x - (finger_width - monitor.width / scaling) / 2),
+        scaling * (y - (finger_height - monitor.height / scaling) / 2)
     )
 
 
 def open_eventfile(args):
-    """ssh to reMarkable and open event0"""
+    """ssh to reMarkable and open event1"""
 
     if args.key is not None:
         password = None
@@ -90,7 +92,7 @@ def open_eventfile(args):
     print("Connected to {}".format(args.address))
 
     # Start reading events
-    _, stdout, _ = client.exec_command('cat /dev/input/event0')
+    _, stdout, _ = client.exec_command('cat /dev/input/event1')
 
     return stdout
 
@@ -99,6 +101,9 @@ def read_tablet(args):
     """Loop forever and map evdev events to mouse"""
 
     new_x = new_y = False
+    last_x = last_y = None
+    x = y = temp_x = temp_y = 0
+    moved = False
 
     monitor = get_monitors()[args.monitor]
     log.debug('Chose monitor: {}'.format(monitor))
@@ -108,36 +113,68 @@ def read_tablet(args):
     while True:
         _, _, e_type, e_code, e_value = struct.unpack('2IHHi', stdout.read(16))
 
+
         if e_type == e_type_abs:
 
             # handle x direction
-            if e_code == e_code_stylus_xpos:
+            if e_code == e_code_finger_xpos:
                 log.debug(f'{e_value}')
                 x = e_value
                 new_x = True
+                if last_x is not None:
+                    temp_x += x - last_x
+                last_x = x
 
             # handle y direction
-            if e_code == e_code_stylus_ypos:
+            elif e_code == e_code_finger_ypos:
                 log.debug(f'\t{e_value}')
                 y = e_value
                 new_y = True
+                if last_y is not None:
+                    temp_y += y - last_y
+                    print(last_y, y, temp_y)
+                    moved = True
+                else:
+                    print(' . ', y, temp_y)
+                last_y = y
 
             # handle draw
-            if e_code == e_code_stylus_pressure:
+            elif e_code == e_code_finger_pressure:
                 log.debug(f'\t\t{e_value}')
                 if e_value > args.threshold:
                     mouse.press(Button.left)
                 else:
                     mouse.release(Button.left)
 
+            elif e_code == e_code_finger_touch:
+                log.debug('\t\t\tup')
+                if e_value == e_value_finger_up:
+                    last_x = None
+                    last_y = None
+                    # if the mouse hasn't moved since the last finger lift, send a click
+                    print(' . ', x, temp_x, moved, 'u')
+                    if not moved:
+                        mouse.press(Button.left)
+                        mouse.release(Button.left)
+
+                    moved = False
+
+
             # only move when x and y are updated for smoother mouse
-            if new_x and new_y:
-                mapped_x, mapped_y = fit(x, y, stylus_width, stylus_height, monitor, args.orientation)
+            if new_y or new_x:
+                mapped_x, mapped_y = fit(
+                    temp_x, temp_y,
+                    finger_width, finger_height,
+                    monitor,
+                    args.orientation
+                )
                 mouse.move(
                     monitor.x + mapped_x - mouse.position[0],
                     monitor.y + mapped_y - mouse.position[1]
                 )
+
                 new_x = new_y = False
+                # last_x, last_y = x, y
 
 def main():
 
