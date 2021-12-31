d93addc8d8e309edce5787f4aa5d8e6f26d6d715
Marco Bazzani <marcobazzani@Marcos-MacBook-Pro.local>
2021-12-31 15:26:25 +0100

    initial 2 finger scroll support

---
+++ remarkable_mouse/ft5406.py
@@ -0,0 +1,227 @@
+import glob
+import io
+import os
+import errno
+import struct
+from collections import namedtuple
+import threading
+import time
+import select
+import queue
+
+TOUCH_X = 0
+TOUCH_Y = 1
+
+TouchEvent = namedtuple('TouchEvent', ('timestamp', 'type', 'code', 'value'))
+
+EV_SYN = 0
+EV_ABS = 3
+
+ABS_X = 0
+ABS_Y = 1
+
+ABS_MT_SLOT = 0x2f # 47 MT slot being modified
+ABS_MT_POSITION_X = 0x35 # 53 Center X of multi touch position
+ABS_MT_POSITION_Y = 0x36 # 54 Center Y of multi touch position
+ABS_MT_TRACKING_ID = 0x39 # 57 Unique ID of initiated contact
+
+TS_PRESS = 1
+TS_RELEASE = 0
+TS_MOVE = 2
+
+class Touch(object):
+    def __init__(self, slot, x, y):
+        self.slot = slot
+
+        self._x = x
+        self._y = y
+        self.last_x = -1
+        self.last_y = -1
+
+        self._id = -1
+        self.events = []
+        self.on_move = None
+        self.on_press = None
+        self.on_release = None
+        
+    @property
+    def position(self):
+        return (self.x, self.y)
+
+    @property
+    def last_position(self):
+        return (self.last_x, self.last_y)
+
+    @property
+    def valid(self):
+        return self.id > -1
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, value):
+        if value != self._id:
+            if value == -1 and not TS_RELEASE in self.events:
+                self.events.append(TS_RELEASE)    
+            elif not TS_PRESS in self.events:
+                self.events.append(TS_PRESS)
+
+        self._id = value
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if value != self._x and not TS_MOVE in self.events:
+            self.events.append(TS_MOVE)
+        self.last_x = self._x
+        self._x = value
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        if value != self._y and not TS_MOVE in self.events:
+            self.events.append(TS_MOVE)
+        self.last_y = self._y
+        self._y = value
+
+    def handle_events(self, fingers):
+        """Run outstanding press/release/move events"""
+        for event in self.events:
+            if event == TS_MOVE and callable(self.on_move):
+                self.on_move(event, self, fingers)
+            if event == TS_PRESS and callable(self.on_press):
+                fingers+=1
+                self.on_press(event, self, fingers)
+            if event == TS_RELEASE and callable(self.on_release):
+                fingers-=1
+                self.on_release(event, self, fingers)
+        self.events = []
+        return fingers
+
+
+class Touches(list):
+    @property
+    def valid(self):
+        return [touch for touch in self if touch.valid]
+
+class Touchscreen(object):
+
+    TOUCHSCREEN_EVDEV_NAME = 'FT5406 memory based driver'
+    EVENT_FORMAT = str('2IHHi')
+    EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
+
+    def __init__(self, device=None, f_channel=None, f_stream=None):
+        self._device = self.TOUCHSCREEN_EVDEV_NAME if device is None else device
+        self._running = False
+        self._thread = None
+        self._f_poll = select.poll()
+
+        if f_channel and f_stream:
+            self._f_device = f_channel
+            self._f_stream = f_stream
+        else:
+            self._f_device = io.open(self._touch_device(), 'rb', self.EVENT_SIZE)
+
+        self._f_poll.register(self._f_device, select.POLLIN)
+        self.position = Touch(0, 0, 0)
+        self.touches = Touches([Touch(x, 0, 0) for x in range(11)])
+        self._event_queue = queue.Queue()
+        self._touch_slot = 0
+        self.fingers = 0
+
+    def _run(self):
+        self._running = True
+        while self._running:
+            self.poll()
+            #time.sleep(0.0001)
+
+    def run(self):
+        if self._thread is not None:
+            return
+
+        self._thread = threading.Thread(target=self._run)
+        self._thread.start()
+
+    def stop(self):
+        if self._thread is None:
+            return
+ 
+        self._running = False
+        self._thread.join()
+        self._thread = None
+
+    @property
+    def _current_touch(self):
+        return self.touches[self._touch_slot]
+
+    def close(self):
+        self._f_device.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.close()
+
+    def __iter__(self):
+        pass
+
+    def _lazy_read(self):
+        while self._wait_for_events():
+            event = self._f_stream.read(self.EVENT_SIZE)
+            if not event:
+                break
+            yield event
+
+    def _get_pending_events(self):
+        for event in self._lazy_read():
+            (tv_sec, tv_usec, type, code, value) = struct.unpack(self.EVENT_FORMAT, event)
+            self._event_queue.put(TouchEvent(tv_sec + (tv_usec / 1000000), type, code, value))
+
+    def _wait_for_events(self, timeout=2):
+        return self._f_poll.poll(timeout)
+
+    def poll(self):
+        self._get_pending_events()
+
+        while not self._event_queue.empty():
+            event = self._event_queue.get()
+            self._event_queue.task_done()
+
+            if event.type == EV_SYN: # Sync
+                for touch in self.touches:
+                    self.fingers = touch.handle_events(self.fingers)
+                return self.touches
+                
+            if event.type == EV_ABS: # Absolute cursor position
+                if event.code == ABS_MT_SLOT:
+                    self._touch_slot = event.value
+            
+                if event.code == ABS_MT_TRACKING_ID: 
+                    self._current_touch.id = event.value
+            
+                if event.code == ABS_MT_POSITION_X:
+                    self._current_touch.x = event.value
+            
+                if event.code == ABS_MT_POSITION_Y:
+                    self._current_touch.y = event.value
+            
+                if event.code == ABS_X:
+                    self.position.x = event.value
+            
+                if event.code == ABS_Y:
+                    self.position.y = event.value
+
+        return []
+
+
+    def read(self):
+        return next(iter(self))
--- remarkable_mouse/pynput.py
+++ remarkable_mouse/pynput.py
@@ -1,11 +1,16 @@
 import logging
 import struct
+import threading
 from screeninfo import get_monitors
+from pynput.mouse import Button, Controller
 
-from .common import get_monitor
+from remarkable_mouse.ft5406 import Touchscreen, TS_MOVE, TS_PRESS, TS_RELEASE
+
 
 logging.basicConfig(format='%(message)s')
 log = logging.getLogger('remouse')
+
+# see https://github.com/canselcik/libremarkable/blob/master/src/input/ecodes.rs
 
 # evtype_sync = 0
 # evtype_key = 1
@@ -17,9 +22,10 @@ e_type_abs = 3
 e_code_stylus_xpos = 1
 e_code_stylus_ypos = 0
 e_code_stylus_pressure = 24
-# evcode_finger_xpos = 53
-# evcode_finger_ypos = 54
-# evcode_finger_pressure = 58
+evcode_finger_xpos = 53
+evcode_finger_ypos = 54
+evcode_finger_pressure = 58 
+evcode_finger_count = 57  
 
 # wacom digitizer dimensions
 wacom_width = 15725
@@ -47,66 +53,107 @@ def remap(x, y, wacom_width, wacom_height, monitor_width,
     ratio_width, ratio_height = monitor_width / wacom_width, monitor_height / wacom_height
 
     if mode == 'fill':
-        scaling_x = max(ratio_width, ratio_height)
-        scaling_y = scaling_x
+        scaling = max(ratio_width, ratio_height)
     elif mode == 'fit':
-        scaling_x = min(ratio_width, ratio_height)
-        scaling_y = scaling_x
-    elif mode == 'stretch':
-        scaling_x = ratio_width
-        scaling_y = ratio_height
+        scaling = min(ratio_width, ratio_height)
     else:
         raise NotImplementedError
 
     return (
-        scaling_x * (x - (wacom_width - monitor_width / scaling_x) / 2),
-        scaling_y * (y - (wacom_height - monitor_height / scaling_y) / 2)
+        scaling * (x - (wacom_width - monitor_width / scaling) / 2),
+        scaling * (y - (wacom_height - monitor_height / scaling) / 2)
     )
 
 
-def read_tablet(rm_inputs, *, orientation, monitor_num, region, threshold, mode):
+def read_tablet(rm_inputs, *, orientation, monitor, threshold, mode):
     """Loop forever and map evdev events to mouse
 
     Args:
         rm_inputs (dictionary of paramiko.ChannelFile): dict of pen, button
             and touch input streams
         orientation (str): tablet orientation
-        monitor_num (int): monitor number to map to
-        region (boolean): whether to selection mapping region with region tool
+        monitor (int): monitor number to map to
         threshold (int): pressure threshold
         mode (str): mapping mode
     """
 
-    from pynput.mouse import Button, Controller
+    monitor = get_monitors()[monitor]
+    log.debug('Chose monitor: {}'.format(monitor))
 
+    mouse = threading.Thread(target=move_mouse, args=(rm_inputs, orientation, monitor, threshold, mode))
+    gesture = threading.Thread(target=do_gesture, args=(rm_inputs, orientation, monitor, threshold, mode))
+    mouse.daemon = True
+    gesture.daemon = True
+    mouse.start()
+    gesture.start()
+    mouse.join()
+    gesture.join()
+
+def do_gesture(rm_inputs, orientation, monitor, threshold, mode):
+    mouse = Controller()
+    import signal
+
+    ts = Touchscreen("pt_mt", rm_inputs['touch'].channel, rm_inputs['touch'])
+
+    def handle_event(event, touch, fingers):
+        px, py = remap(
+                *touch.position,
+                wacom_width, wacom_height,
+                monitor.width, monitor.height,
+                mode, orientation
+            )
+        lpx, lpy = remap(
+                *touch.last_position,
+                wacom_width, wacom_height,
+                monitor.width, monitor.height,
+                mode, orientation
+            )
+
+        if touch.slot == 0 and fingers == 2:
+            mouse.scroll(px-lpx, py-lpy)
+
+    for touch in ts.touches:
+        touch.on_press = handle_event
+        touch.on_release = handle_event
+        touch.on_move = handle_event
+
+    ts.run()
+
+    try:
+        signal.pause()
+    except KeyboardInterrupt:
+        print("Stopping thread...")
+        ts.stop()
+        exit()
+    
+                
+
+def move_mouse(rm_inputs, orientation, monitor, threshold, mode):
     lifted = True
     new_x = new_y = False
 
     mouse = Controller()
 
-    monitor = get_monitor(monitor_num, region, orientation)
-    log.debug('Chose monitor: {}'.format(monitor))
-
     while True:
         _, _, e_type, e_code, e_value = struct.unpack('2IHHi', rm_inputs['pen'].read(16))
+        log.debug(f'PEN: {e_type:02x} {e_code:02x} {e_value:02x} \t###\t {e_type} {e_code} {e_value}')
 
         if e_type == e_type_abs:
-
             # handle x direction
             if e_code == e_code_stylus_xpos:
-                log.debug(e_value)
+                #log.debug(e_value)
                 x = e_value
                 new_x = True
 
             # handle y direction
             if e_code == e_code_stylus_ypos:
-                log.debug('\t{}'.format(e_value))
+                #log.debug('\t{}'.format(e_value))
                 y = e_value
                 new_y = True
 
             # handle draw
             if e_code == e_code_stylus_pressure:
-                log.debug('\t\t{}'.format(e_value))
+                #log.debug('\t\t{}'.format(e_value))
                 if e_value > threshold:
                     if lifted:
                         log.debug('PRESS')
@@ -132,3 +179,6 @@ def read_tablet(rm_inputs, *, orientation, monitor_num, region, threshold, mode)
                     monitor.y + mapped_y - mouse.position[1]
                 )
                 new_x = new_y = False
+
+
+
